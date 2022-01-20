ea952f85cdf0dcdea3c2ae8c2f71006f47e491af
SangChul1 <98008442+SangChul1@users.noreply.github.com>
2022-01-20 18:13:08 +0900

    Update 박상철_Ext1.py

--- MSA20220119/박상철_Ext1.py
+++ MSA20220119/박상철_Ext1.py
@@ -19,19 +19,46 @@ total1=0
 total2=0
 total3=0
 total4=0
+
+for AvgData in 홍길동1: 
+    name1 = '홍길동1'
+    total1 = total1 + 홍길동1[AvgData]
+    value1 = total1 / len(홍길동1)
+print('이 름', name1)
+print('총 점:', total1)
+print('평 균:', value1)
+
+print()
+
 for AvgData in 홍길동2: 
+    name2 = '홍길동2'
     total2 = total2 + 홍길동2[AvgData]
     value2 = total2 / len(홍길동2)
+print('이 름', name2)
+print('총 점:', total2)
+print('평 균:', value2)
+
+print()
 
 for AvgData in 홍길동3: 
+    name3 = '홍길동3'
     total3 = total3 + 홍길동3[AvgData]
     value3 = total3 / len(홍길동3)
-
+print('이 름', name3)
 print('총 점:', total3)
 print('평 균:', value3)
+
+print()
+
+for AvgData in 홍길동4: 
+    name4 = '홍길동4'
+    total4 = total4 + 홍길동4[AvgData]
+    value4 = total4 / len(홍길동4)
+print('이 름', name4)
+print('총 점:', total4)
+print('평 균:', value4)
 
 #for AvgData in (Score1, Score2,Score3,Score4):
 
 #print('총 점:', total3)
 #print('평 균:', value3)
-논리적인사고가 안되네요 ... 조금 복잡해졌다고...
